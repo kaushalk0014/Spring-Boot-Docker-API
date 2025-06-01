ae94704439f07c8dfd430d0411701d8086296711
kaushal kumar <kaushal.k.0014@gmail.com>
2025-06-01 21:35:54 +0530

    enable cors

--- src/main/java/com/learning/docker/config/SpringBootDockerApiApplication.java
+++ src/main/java/com/learning/docker/config/SpringBootDockerApiApplication.java
@@ -2,7 +2,10 @@ package com.learning.docker.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ComponentScan(basePackages = {"com.learning.docker"})
 @SpringBootApplication
@@ -11,5 +14,18 @@ public class SpringBootDockerApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApiApplication.class, args);
 	}
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("http://localhost:3000") // React origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
